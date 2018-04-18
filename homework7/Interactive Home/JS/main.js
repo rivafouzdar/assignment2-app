b6e2dbb2a72c48a57093f60e24489b73d0332d75
rivafouzdar <rvf@andrew.cmu.edu>
2018-04-18 14:49:32 -0400

    4/18 3pm

--- homework7/Interactive Home/JS/main.js
+++ homework7/Interactive Home/JS/main.js
@@ -1,20 +1,23 @@
 d3.select("#circleList")
   .append("svg")
   .attr("width", 1200).attr("height", 730)
-  .append("g")
-  //echo
+  .append("g");
+
+//echo
+d3.select("g")
   .append("circle")
   .attr("cx", 440).attr("cy", 600)
   .attr("r", 10)
   .attr("position", "absolute")
-  .attr("fill", "red");
+  .attr("fill", "red")
+  .attr("text", "Amazon Echo");
 
 //smartBulb
 d3.select("g")
   .append("circle")
   .attr("cx", 512).attr("cy", 345)
   .attr("r", 10)
-  .attr("fill", "red");
+  .attr("fill", "red")
   .attr("text", "SmartBulb");
 
 //roomba
@@ -30,7 +33,8 @@ d3.select("g")
   .append("circle")
   .attr("cx", 765).attr("cy", 350)
   .attr("r", 10)
-  .attr("fill", "red");
+  .attr("fill", "red")
+  .attr("text", "Nest");
 
 //homeSecurity
 d3.select("g")
@@ -41,9 +45,10 @@ d3.select("g")
   .attr("text", "Home Security System");
 
 d3.selectAll("circle")
+  .on("click", click)
   .on("mouseover", mouseover)
-  .append("svg:title")
-  .text("circless");
+  .append("svg:title");
+
 
 var tooltip = d3.select("#circleList")
                 .append("div")
@@ -59,89 +64,14 @@ function mouseover(d) {
              .style("left", event.pageX+"px");
 }
 
+function click(d) {
 
-// window.onload = function() {
+      var modal = document.getElementById("myModalECHO");
+      modal.style.display = "block";
 
-//   // Get the button that opens the echo modal
-//   var echo = document.getElementById("echo");
-
-//   // When the user clicks the button, open the modal
-//   echo.onclick = function() {
-//       var modal = document.getElementById("myModalECHO");
-//       modal.style.display = "block";
-
-//       var span = document.getElementById("closeECHO");
-//       span.onclick = function() {
-//         var modal = document.getElementById("myModalECHO");
-//         modal.style.display = "none";
-//       }
-//   }
-
-//   // Get the button that opens the bulb modal
-//   var bulb = document.getElementById("bulb");
-
-//   // When the user clicks the button, open the modal
-//   bulb.onclick = function() {
-//       var modal = document.getElementById("myModalBULB");
-//       modal.style.display = "block";
-
-//       var span = document.getElementById("closeBULB");
-//       span.onclick = function() {
-//         var modal = document.getElementById("myModalBULB");
-//         modal.style.display = "none";
-//       }
-//   }
-
-//   // Get the button that opens the echo modal
-//   var roomba = document.getElementById("roomba");
-
-//   // When the user clicks the button, open the modal
-//   roomba.onclick = function() {
-//       var modal = document.getElementById("myModalROOMBA");
-//       modal.style.display = "block";
-
-//       var span = document.getElementById("closeROOMBA");
-//       span.onclick = function() {
-//         var modal = document.getElementById("myModalROOMBA");
-//         modal.style.display = "none";
-//       }
-//   }
-
-//   // Get the button that opens the echo modal
-//   var nest = document.getElementById("nest");
-
-//   // When the user clicks the button, open the modal
-//   nest.onclick = function() {
-//       var modal = document.getElementById("myModalNEST");
-//       modal.style.display = "block";
-
-//       var span = document.getElementById("closeNEST");
-//       span.onclick = function() {
-//         var modal = document.getElementById("myModalNEST");
-//         modal.style.display = "none";
-//       }
-//   }
-
-//   // Get the button that opens the echo modal
-//   var security = document.getElementById("security");
-
-//   // When the user clicks the button, open the modal
-//   security.onclick = function() {
-//       var modal = document.getElementById("myModalSECURITY");
-//       modal.style.display = "block";
-
-//       var span = document.getElementById("closeSECURITY");
-//       span.onclick = function() {
-//         var modal = document.getElementById("myModalSECURITY");
-//         modal.style.display = "none";
-//       }
-//   }
-
-
-//   // // When the user clicks anywhere outside of the modal, close it
-//   // window.onclick = function(event) {
-//   //     if (event.target == modal) {
-//   //         modal.style.display = "none";
-//   //     }
-//   // }
-// };
+      var span = document.getElementById("closeECHO");
+      span.onclick = function() {
+        var modal = document.getElementById("myModalECHO");
+        modal.style.display = "none";
+      }
+}
