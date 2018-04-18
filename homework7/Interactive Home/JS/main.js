98af98e753828555b545df6b29c0d81a4d60a964
rivafouzdar <rvf@andrew.cmu.edu>
2018-04-18 15:41:57 -0400

    4/18 4pm

--- homework7/Interactive Home/JS/main.js
+++ homework7/Interactive Home/JS/main.js
@@ -1,3 +1,5 @@
+var curModal = "None";
+
 d3.select("#circleList")
   .append("svg")
   .attr("width", 1200).attr("height", 730)
@@ -10,6 +12,7 @@ d3.select("g")
   .attr("r", 10)
   .attr("position", "absolute")
   .attr("fill", "red")
+  .attr("curModal", "echo")
   .attr("text", "Amazon Echo");
 
 //smartBulb
@@ -18,6 +21,7 @@ d3.select("g")
   .attr("cx", 512).attr("cy", 345)
   .attr("r", 10)
   .attr("fill", "red")
+  .attr("curModal", "bulb")
   .attr("text", "SmartBulb");
 
 //roomba
@@ -26,6 +30,7 @@ d3.select("g")
   .attr("cx", 940).attr("cy", 550)
   .attr("r", 10)
   .attr("fill", "red")
+  .attr("curModal", "roomba")
   .attr("text", "Roomba");
 
 //nest
@@ -34,6 +39,7 @@ d3.select("g")
   .attr("cx", 765).attr("cy", 350)
   .attr("r", 10)
   .attr("fill", "red")
+  .attr("curModal", "nest")
   .attr("text", "Nest");
 
 //homeSecurity
@@ -42,6 +48,7 @@ d3.select("g")
   .attr("cx", 1060).attr("cy", 390)
   .attr("r", 10)
   .attr("fill", "red")
+  .attr("curModal", "security")
   .attr("text", "Home Security System");
 
 d3.selectAll("circle")
@@ -62,10 +69,13 @@ function mouseover(d) {
              .text(d3.select(this).attr("text"))
              .style("top", event.pageY+"px")
              .style("left", event.pageX+"px");
+    curModal = d3.select(this).attr("curModal");
+    console.log(curModal);
 }
 
 function click(d) {
 
+    if (curModal == "echo") {
       var modal = document.getElementById("myModalECHO");
       modal.style.display = "block";
 
@@ -74,4 +84,49 @@ function click(d) {
         var modal = document.getElementById("myModalECHO");
         modal.style.display = "none";
       }
+    }
+
+    if (curModal == "bulb") {
+      var modal = document.getElementById("myModalBULB");
+      modal.style.display = "block";
+
+      var span = document.getElementById("closeBULB");
+      span.onclick = function() {
+        var modal = document.getElementById("myModalBULB");
+        modal.style.display = "none";
+      }
+    }
+
+    if (curModal == "roomba") {
+      var modal = document.getElementById("myModalROOMBA");
+      modal.style.display = "block";
+
+      var span = document.getElementById("closeROOMBA");
+      span.onclick = function() {
+        var modal = document.getElementById("myModalROOMBA");
+        modal.style.display = "none";
+      }
+    }
+
+    if (curModal == "nest") {
+      var modal = document.getElementById("myModalNEST");
+      modal.style.display = "block";
+
+      var span = document.getElementById("closeNEST");
+      span.onclick = function() {
+        var modal = document.getElementById("myModalNEST");
+        modal.style.display = "none";
+      }
+    }
+
+    if (curModal == "security") {
+      var modal = document.getElementById("myModalSECURITY");
+      modal.style.display = "block";
+
+      var span = document.getElementById("closeSECURITY");
+      span.onclick = function() {
+        var modal = document.getElementById("myModalSECURITY");
+        modal.style.display = "none";
+      }
+    }
 }
